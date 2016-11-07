e1b13720c010b7e288e71d44a636d6dc104fb8ee
Kenneth Massada <kmassada@gmail.com>
2016-11-07 02:33:58 -0500

    notification service

---
+++ app/services/notification/notification.service.ts
@@ -0,0 +1,52 @@
+import { ReplaySubject } from "rxjs";
+import { List } from "immutable";
+
+import { SocketService, ISocketItem } from "../socket";
+
+import { INotification } from "../../models/notification.model";
+
+export class NotificationService {
+    notifications: ReplaySubject<any> = new ReplaySubject(1);
+    private list: List<any> = List();
+    private socketService: SocketService;
+
+    /**
+     * Constructor.
+     *
+     * @class NotificationService
+     * @constructor
+     * @param stream string
+     */
+    constructor(private stream: string) {
+        this.socketService = new SocketService();
+        this.socketService
+            .get("notifications/" + encodeURIComponent(this.stream))
+            .subscribe(
+                (socketItem: ISocketItem) => {
+                    let notification: INotification = socketItem.item;
+                    this.list = this.list.push(notification);
+                    this.notifications.next(this.list);
+                },
+                error => console.log(error)
+            );
+    }
+
+    /**
+     * Emit notification using socket service
+     *
+     * @class NotificationService
+     * @method create
+     * @param from string
+     * @param notification string
+     * @return void
+     */
+    create(from: string, notification: string): void {
+        this.socketService.socket.emit("create", {
+            stream: this.stream,
+            created: new Date(),
+            from: from,
+            to: "",
+            notification: notification
+        });
+    }
+}
